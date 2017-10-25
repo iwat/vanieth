916cf30213f6b1946424873df835f6e09e3629b9
iwat <iwatth@gmail.com>
2017-10-25 11:57:59 +0700

    Search for prefix and suffix

--- vanieth.go
+++ vanieth.go
@@ -11,7 +11,10 @@ import (
 	"github.com/ethereum/go-ethereum/crypto"
 )
 
-func find(toMatch string, done, tick chan<- bool, stop <-chan bool) {
+func find(leading, trailing string, done, tick chan<- bool, stop <-chan bool) {
+	leading = strings.ToLower(leading)
+	trailing = strings.ToLower(trailing)
+
 	loops := 0
 	for true {
 		select {
@@ -19,9 +22,9 @@ func find(toMatch string, done, tick chan<- bool, stop <-chan bool) {
 			done <- true
 		default:
 			key, _ := crypto.GenerateKey()
-			addr := crypto.PubkeyToAddress(key.PublicKey)
-			addrStr := hex.EncodeToString(addr[:])
-			if compare(addrStr, toMatch, key) {
+			binAddr := crypto.PubkeyToAddress(key.PublicKey)
+			addr := hex.EncodeToString(binAddr[:])
+			if compare(addr, leading, trailing, key) {
 				done <- true
 			}
 		}
@@ -33,17 +36,15 @@ func find(toMatch string, done, tick chan<- bool, stop <-chan bool) {
 	}
 }
 
-func compare(addrStr string, toMatch string, key *ecdsa.PrivateKey) bool {
-	toMatch = strings.ToLower(toMatch)
-	addrStrMatch := strings.TrimPrefix(addrStr, toMatch)
-	found := addrStrMatch != addrStr
+func compare(addr, leading, trailing string, key *ecdsa.PrivateKey) bool {
+	found := addr[0:len(leading)] == leading && addr[len(addr)-len(trailing):len(addr)] == trailing
 
 	if !found {
 		return false
 	}
 
 	keyStr := hex.EncodeToString(crypto.FromECDSA(key))
-	addrFound(addrStr, keyStr)
+	addrFound(addr, keyStr)
 	return true
 }
 
@@ -56,14 +57,18 @@ func main() {
 		os.Exit(1)
 	}
 
-	toMatch := os.Args[1]
+	leading := os.Args[1]
+	trailing := ""
+	if len(os.Args) >= 3 {
+		trailing = os.Args[2]
+	}
 
 	done := make(chan bool, numWorkers)
 	tick := make(chan bool)
 	stop := make(chan bool, numWorkers)
 
 	for i := 0; i < numWorkers; i++ {
-		go find(toMatch, done, tick, stop)
+		go find(leading, trailing, done, tick, stop)
 	}
 
 	loops := 0
@@ -92,15 +97,15 @@ loop:
 	close(stop)
 }
 
-func addrFound(addrStr string, keyStr string) {
+func addrFound(addr string, keyStr string) {
 	println("Address found:")
-	fmt.Printf("addr: 0x%s\n", addrStr)
+	fmt.Printf("addr: 0x%s\n", addr)
 	fmt.Printf("pvt: 0x%s\n", keyStr)
 	println("\nexiting...")
 }
 
 func errNoArg() {
-	println("You need to pass a vanity match, retry with an extra agrument like: 42")
-	println("\nexample: go run vanieth.go 42")
+	println("You need to pass a vanity match, retry with an extra agrument like: 42 42")
+	println("\nexample: go run vanieth.go 42 42")
 	println("\nexiting...")
 }
